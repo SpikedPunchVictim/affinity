59b74545776b4dde3cfaec72101b87f159c7d794
William Hoopes <whoopes@kixeye.com>
2015-07-01 13:11:17 -0700

    Next round

--- main.js
+++ main.js
@@ -4,11 +4,22 @@ var QualifiedObject = require('./lib/qualifiedObject.js');
 var gaia = require('./lib/index.js');
 
 var proj = gaia.create();
+var root = proj.root;
 
-proj.root.on('items-adding', function(items) {
+proj.root.children.on('items-adding', function(items) {
     console.log('Namespace <Root: items-adding>: %j', items);
 });
 
-proj.root.add('Items');
+proj.root.children.add('Items');
 
 
+var item = root.models.add('Item');
+item.members.add('name', 'string', 'default');
+item.members.add('cost', 'decimal', '0.0')
+item.members.add('canSell', 'bool', 'true');
+
+
+var iceSword = root.instances.add('Ice Sword', item);
+iceSword.fields.set('name', 'Ice Sword');
+var name = iceSword.fields.get('name');
+iceSwords.reset('name'); // returns to default

a03c5d205fa3741c55f3710f0efae55764c60d79
SpikedPunchVictim <qof_4@hotmail.com>
2015-11-15 14:50:05 -0800

    incremental

--- main.js
+++ main.js
@@ -13,24 +13,16 @@ var types = gaia.types;
 
 
 var o = ObservableCollection.create();
-o.add(5, 3, 3, 4, 2, 6);
-expect(o.length).to.be.equal(6);
-o.remove(4);
-expect(o.length).to.be.equal(5);
-expect(o).to.be.eql([5, 3, 3, 2, 6]);
-o.remove(3, 6);
-expect(o.length).to.be.equal(3);
-expect(o).to.be.eql([5, 3, 2]);
+o.add(1, 2, 3, 4, 5, 6);
+//expect(o.length).to.equal(5);
+o.copyWithin(1, 3, 5);
+o.remove(1);
+//expect(o.length).to.equal(4);
+o.remove(2);
+//expect(o.length).to.equal(3);
+o.remove(3, 4, 5);
+//expect(o.length).to.equal(0)
 
-o.clear();
-o.fill(5, 0, 3);
-expect(o.length).to.be.equal(0);
-o.push(1, 2, 3, 4);
-o.fill(5, 0, 3);
-expect(o).to.be.eql([5, 5, 5, 5]);
-o.fill(2, 1, 2);
-expect(o.length).to.be.equal(6);
-expect(o).to.be.eql([5, 1, 1, 5, 5, 5]);
 
 
 var proj = gaia.create();
--- test/observableCollections.js
+++ test/observableCollections.js
@@ -1,9 +1,13 @@
+var chai = require('chai')
 var expect = require('chai').expect;
 var assert = require('chai').assert;
+var spies = require('chai-spies');
 var util = require('util');
 var EventEmitter = require('events').EventEmitter;
 var helpers = require('../lib/helpers.js');
 var ObservableCollection = require('../lib/collections/observableCollection.js');
+
+chai.use(spies);
 
 describe('ObservableCollection', function() {
 
@@ -42,10 +46,10 @@ describe('ObservableCollection', function() {
         expect(o.length).to.be.equal(0);
         o.push(1, 2, 3, 4);
         o.fill(5, 0, 3);
-        expect(o).to.be.eql([5, 5, 5, 5]);
+        expect(o).to.be.eql([5, 5, 5, 4]);
         o.fill(2, 1, 2);
-        expect(o.length).to.be.equal(6);
-        expect(o).to.be.eql([5, 1, 1, 5, 5, 5]);
+        expect(o.length).to.be.equal(4);
+        expect(o).to.be.eql([5, 2, 5, 4]);
     });
 
     it('supports copyWithin()', () => {
@@ -55,9 +59,9 @@ describe('ObservableCollection', function() {
         o.copyWithin(1, 2, 4);
         expect(o).to.be.eql([ 1, 3, 4, 4, 5, 6 ]);
         o.copyWithin(0, -3, 5);
-        expect(o).to.be.eql([ 5, 5, 4, 5, 5, 6 ]);
+        expect(o).to.be.eql([  4, 5, 4, 4, 5, 6 ]);
         o.copyWithin(0, 2, -1);
-        expect(o).to.be.eql([ 4, 5, 5, 5, 5, 6 ]);
+        expect(o).to.be.eql([ 4, 4, 5, 4, 5, 6 ]);
     });
 
     it('supports pop()', () => {
@@ -92,18 +96,6 @@ describe('ObservableCollection', function() {
         expect(o.shift()).to.be.undefined;
     });
 
-    it('supports slice()', () => {
-        var o = ObservableCollection.create();
-        expect(o.push(1, 2, 3, 4, 5, 6)).to.be.equal(6);
-        expect(o.slice(2)).to.be.eql([3, 4, 5, 6]);
-        expect(o).to.be.eql([1, 2, 3, 4, 5, 6]);
-        expect(o.slice(-3)).to.be.eql([4, 5, 6])
-        o.clear();
-        o.push(1, 2, 3, 4, 5, 6);
-        expect(o.slive(-4, 5)).to.be.eql(3, 4, 5);
-        expect(o).to.be.eql([1, 2, 3, 4, 5, 6])
-    });
-
     it('supports splice()', () => {
         var o = ObservableCollection.create();
 
@@ -120,6 +112,9 @@ describe('ObservableCollection', function() {
 
         // Adding items
         o.clear();
+        o.splice(0, 0, 1, 2, 3, 4, 5, 6);
+        expect(o).to.be.eql([1, 2, 3, 4, 5, 6]);
+        o.clear()
         o.push(1, 2, 3, 4, 5, 6);
         expect(o.splice(1, 0, 7, 8, 9)).to.be.eql([]);
         expect(o).to.be.eql([ 1, 7, 8, 9, 2, 3, 4, 5, 6 ]);
@@ -146,14 +141,135 @@ describe('ObservableCollection', function() {
     it('supports unshift()', () => {
         var o = ObservableCollection.create();
         expect(o.unshift(1, 2, 3, 4, 5, 6)).to.be.equal(6);
-        expect(o.unshift(0, 0)).to.be.eql(9);
+        expect(o.unshift(0, 0)).to.be.eql(8);
         expect(o).to.be.eql([0, 0, 1, 2, 3, 4, 5, 6]);
     });
 
     describe('#Events', () => {
+        var tests = [
+            {
+                'desc': 'adding on add()',
+                'sub': (o, cb) => o.on('adding', cb),
+                'act': o => o.add(1, 2, 3)
+            },
+            {
+                'desc': 'added on add()',
+                'sub': (o, cb) => o.on('added', cb),
+                'act': o => o.add(1, 2, 3)
+            },
+            {
+                'desc': 'removing on remove()',
+                'sub': (o, cb) => o.on('removing', cb),
+                'act': o => { o.add(1, 2, 3); o.remove(1); }
+            },
+            {
+                'desc': 'removed on remove()',
+                'sub': (o, cb) => o.on('removed', cb),
+                'act': o => { o.add(1, 2, 3); o.remove(1); }
+            },
+            {
+                'desc': 'removing on clear()',
+                'sub': (o, cb) => o.on('removing', cb),
+                'act': o => { o.add(1, 2, 3); o.clear(); }
+            },
+            {
+                'desc': 'removed on clear()',
+                'sub': (o, cb) => o.on('removed', cb),
+                'act': o => { o.add(1, 2, 3); o.clear(); }
+            },
+            {
+                'desc': 'replacing on fill()',
+                'sub': (o, cb) => o.on('replacing', cb),
+                'act': o => { o.add(1, 2, 3); o.fill(4, 0, 2); }
+            },
+            {
+                'desc': 'replaced on fill()',
+                'sub': (o, cb) => o.on('replaced', cb),
+                'act': o => { o.add(1, 2, 3); o.fill(1, 3); }
+            },
+            {
+                'desc': 'removing on pop()',
+                'sub': (o, cb) => o.on('removing', cb),
+                'act': o => { o.add(1, 2, 3); o.pop(); }
+            },
+            {
+                'desc': 'removed on pop()',
+                'sub': (o, cb) => o.on('removed', cb),
+                'act': o => { o.add(1, 2, 3); o.pop(); }
+            },
+            {
+                'desc': 'adding on push()',
+                'sub': (o, cb) => o.on('adding', cb),
+                'act': o => { o.push(1, 2, 3); }
+            },
+            {
+                'desc': 'added on push()',
+                'sub': (o, cb) => o.on('added', cb),
+                'act': o => { o.push(1, 2, 3); }
+            },
+            {
+                'desc': 'moving on reverse()',
+                'sub': (o, cb) => o.on('moving', cb),
+                'act': o => { o.add(1, 2, 3); o.reverse(); }
+            },
+            {
+                'desc': 'moved on reverse()',
+                'sub': (o, cb) => o.on('moved', cb),
+                'act': o => { o.add(1, 2, 3); o.reverse(); }
+            },
+            {
+                'desc': 'removing on shift()',
+                'sub': (o, cb) => o.on('removing', cb),
+                'act': o => { o.add(1, 2, 3); o.shift(); }
+            },
+            {
+                'desc': 'removed on shift()',
+                'sub': (o, cb) => o.on('removed', cb),
+                'act': o => { o.add(1, 2, 3); o.shift(); }
+            },
+            {
+                'desc': 'adding on splice()',
+                'sub': (o, cb) => o.on('adding', cb),
+                'act': o => o.splice(0, 0, 1, 2, 3)
+            },
+            {
+                'desc': 'added on splice()',
+                'sub': (o, cb) => o.on('added', cb),
+                'act': o => o.splice(0, 0, 1, 2, 3)
+            },
+            {
+                'desc': 'removing on splice()',
+                'sub': (o, cb) => o.on('removing', cb),
+                'act': o => o.splice(0, 2)
+            },
+            {
+                'desc': 'removed on splice()',
+                'sub': (o, cb) => o.on('removed', cb),
+                'act': o => o.splice(0, 2)
+            },
+            {
+                'desc': 'adding on unshift()',
+                'sub': (o, cb) => o.on('adding', cb),
+                'act': o => o.unshift(0, 2)
+            },
+            {
+                'desc': 'added on unshift()',
+                'sub': (o, cb) => o.on('added', cb),
+                'act': o => o.unshift(0, 2)
+            }
 
+        ];
 
-
+        tests.forEach(test => {
+            it('should emit event ' + test.desc, () => {
+                var spy = chai.spy();
+                var o = ObservableCollection.create();
+                o.add(1, 2, 3, 4, 5, 6);
+                test.sub(o, spy);
+                test.act(o);
+                expect(spy).to.have.been.called();
+            });
+        });
     });
 
     it('should have the correct length when items are removed', function() {
@@ -168,28 +284,6 @@ describe('ObservableCollection', function() {
         expect(o.length).to.equal(0);
     });
 
-    it('should generate the event: adding ', function(done) {
-        var o = ObservableCollection.create();
-        helpers.validateEvent(o, 'adding', function() { o.add(1); }, done);
-    });
-
-    it('should generate the event: added ', function(done) {
-        var o = ObservableCollection.create();
-        helpers.validateEvent(o, 'added', function() { o.add(1); }, done);
-    });
-
-    it('should generate the event: removing ', function(done) {
-        var o = ObservableCollection.create();
-        o.add(1);
-        helpers.validateEvent(o, 'removing', function() { o.remove(1); }, done);
-    });
-
-    it('should generate the event: removed ', function(done) {
-        var o = ObservableCollection.create();
-        o.add(1);
-        helpers.validateEvent(o, 'removed', function() { o.remove(1); }, done);
-    });
-
     it('should be able to use splice to add items', function() {
         // TODO
         //  - Test splice
@@ -197,11 +291,11 @@ describe('ObservableCollection', function() {
         var o = ObservableCollection.create();
         o.splice(0, 0, 1);        
         expect(o.length).to.equal(1);
-        expect(o.at(0)).to.equal(1);
+        expect(o[0]).to.equal(1);
 
         o.splice(1, 0, 2);
         expect(o.length).to.equal(2);
-        expect(o.at(1)).to.equal(2);
+        expect(o[1]).to.equal(2);
     });
 
     it('should be able to use splice to remove items', function() {
@@ -211,11 +305,11 @@ describe('ObservableCollection', function() {
         var o = ObservableCollection.create();
         o.splice(0, 0, 1);
         expect(o.length).to.equal(1);
-        expect(o.at(0)).to.equal(1);
+        expect(o[0]).to.equal(1);
 
         o.splice(1, 0, 2);
         expect(o.length).to.equal(2);
-        expect(o.at(1)).to.equal(2);
+        expect(o[1]).to.equal(2);
     });
 
     it('the splice method should raise events when adding items', function(done) {

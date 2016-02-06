4d4ac34b1adf7f64ceb91c38585f7917cc314303
William Hoopes <whoopes@kixeye.com>
2016-02-05 17:45:07 -0800

    Incremental checkin

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-module.exports.Instance = require('./lib/instance.js');
-module.exports.Field = require('./lib/field.js');
-module.exports.Model = require('./lib/model.js');
-module.exports.Member = require('./lib/member.js');
-module.exports.Namespace = require('./lib/namespace.js');
-module.exports.NamespaceCollection = require('./lib/collections/namespaceCollection.js');
-module.exports.ModelCollection = require('./lib/collections/modelCollection.js');
-module.exports.types = require('./lib/types/index.js');
-module.exports.types.collection = require('./lib/types/collection.js');
-
+'use strict';
+var Instance = require('./lib/instance.js');
+var Field = require('./lib/field.js');
+var Model = require('./lib/model.js');
+var Member = require('./lib/member.js');
+var Namespace = require('./lib/namespace.js');
+var NamespaceCollection = require('./lib/collections/namespaceCollection.js');
+var ModelCollection = require('./lib/collections/modelCollection.js');
+var types = require('./lib/types/index.js');
+types.collection = require('./lib/types/collection.js');
 var Project = require('./lib/project.js');
+var Events = require('./lib/events.js');
+
+module.exports.Instance = Instance;
+module.exports.Field = Field;
+module.exports.Model = Model;
+module.exports.Member = Member;
+module.exports.Namespace = Namespace;
+module.exports.NamespaceCollection = NamespaceCollection;
+module.exports.ModelCollection = ModelCollection;
+module.exports.types = types;
 module.exports.create = Project.create;
+module.exports.Events = Events;
+
+var gaia = {};
+
+for(var exp in module.exports) {
+    gaia[exp] = module.exports[exp];
+}
+
+module.exports.use = function use(plugin) {
+    plugin.register(gaia);
+}

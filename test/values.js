3fb5827ce1066c2109e9e5b6e4409f2d02b31adc
William Hoopes <whoopes@sf.kixeye.com>
2016-10-12 22:17:09 -0700

    Multiple fixes

--- test/values.js
+++ test/values.js
@@ -1,11 +1,14 @@
 'use strict';
 
-var chai = require('chai');
-var expect = chai.expect;
-var gaia = require('../lib/index.js');
-var types = gaia.types;
-var Events = require('../lib/events.js');
-var testing = require('../lib/testing.js');
+var chai = require('chai'),
+   spies = require('chai-spies'),
+   expect = chai.expect,
+   gaia = require('../lib/index.js'),
+   types = gaia.types,
+   Events = require('../lib/events.js'),
+   testing = require('../lib/testing.js');
+
+chai.use(spies);
 
 let Fill = testing.fill;
 
@@ -65,6 +68,7 @@ function sharedBehaviorTest(proxy) {
    });
 }
 
+//---------------------------------------------------------------------
 function primitiveType(proxy) {
    it('should perform an update', function (done) {
       let value = proxy.createValue1();
@@ -74,6 +78,190 @@ function primitiveType(proxy) {
          .then(_ => expect(value.equals(other)).to.be.true)
          .then(_ => done());
    });
+}
+
+//---------------------------------------------------------------------
+function collectionTests(proxy) {
+   let events = types.collection.events;
+   let collection = types.collection;
+   let string = types.string;
+
+   describe('# collection item types', function() {
+      for(let type of Fill.types()) {
+         it(`should have the correct itemType: ${type.toString()}`, function() {
+            let coll = collection.value(type);
+            expect(type.equals(coll.itemType)).to.be.true;
+         });
+      }
+   });
+   
+   it('should be able to iterate over .items', function() {
+      let coll = collection.value(types.string.type());
+      coll.add(string.value('one'), string.value('two'), string.value('three'))
+         .then(_ => {
+            let index = 0;
+            for(let it of coll) {
+               expect(it).to.be.equal(coll.at(index++));
+            }
+         });
+   });
+
+   it('should be able to iterate over the collection value', function() {
+      let coll = collection.value(types.string.type());
+      let value1 = string.value('one');
+      let value2 = string.value('two');
+      let value3 = string.value('three');
+      let values = [value1, value2, value3];
+      coll.add(value1, value2, value3)
+         .then(_ => {
+            let index = 0;
+            for(let it of coll) {
+               expect(it).to.eql(values[index]);
+               ++index;
+            }
+         })
+   });
+
+   it('at() and add()', function(done) {
+      let coll = collection.value(types.string.type());
+      let value1 = string.value('one');
+      let value2 = string.value('two');
+      let value3 = string.value('three');
+      let values = [value1, value2, value3];
+      coll.add(value1, value2, value3)
+         .then(_ => {
+            expect(coll.at(0)).to.eql(value1);
+            expect(coll.at(1)).to.eql(value2);
+            expect(coll.at(2)).to.eql(value3);
+         })
+         .then(_ => done())
+         .catch(err => { done(err) });
+   });
+
+   it('update()', function(done) {
+      let coll = collection.value(types.string.type());
+      let value1 = string.value('one');
+      let value2 = string.value('two');
+      let value3 = string.value('three');
+      let value4 = string.value('four');
+      let value5 = string.value('five');
+      let value6 = string.value('six');
+      let value7 = string.value('seven');
+      let values = [value1, value2, value3];
+      coll.add(value1, value2, value3)
+         .then(_ => {
+            let coll2 = collection.value(string.type());
+            return coll2.add(value4, value5, value6, value7)
+               .then(_ => coll.update(coll2))
+               .then(_ => {
+                  expect(coll.at(0)).to.eql(value4);
+                  expect(coll.at(1)).to.eql(value5);
+                  expect(coll.at(2)).to.eql(value6);
+                  expect(coll.at(3)).to.eql(value7);
+               });
+         })
+         .then(_ => done())
+         .catch(err => done(err));
+   })
+
+//    it('move()', function(done) {
+//       let coll = collection.value(types.string.type());
+//       let value1 = string.value('one');
+//       let value2 = string.value('two');
+//       let value3 = string.value('three');
+
+//       coll.add(value1, value2, value3)
+//          .then(_ => coll.move(0, 1))
+//          .then(_ => {
+//             expect(coll.at(0)).to.equal(value2);
+//             expect(coll.at(1)).to.equal(value1);
+//             return true;
+//          })
+//          .catch(err => done(err));
+//    })
+
+   it('clear()', function(done) {
+      let coll = collection.value(types.string.type());
+      let value1 = string.value('one');
+      let value2 = string.value('two');
+      let value3 = string.value('three');
+
+      coll.add(value1, value2, value3)
+         .then(_ => expect(coll.length).to.be.equal(3))
+         .then(_ => coll.clear())
+         .then(_ => expect(coll.length).to.be.equal(0))
+         .then(_ => done())
+         .catch(err => done(err));
+   })
+
+   it('remove()', function(done) {
+      let coll = collection.value(types.string.type());
+      let value1 = string.value('one');
+      let value2 = string.value('two');
+      let value3 = string.value('three');
+
+      coll.add(value1, value2, value3)
+         .then(_ => expect(coll.length).to.be.equal(3))
+         .then(_ => coll.remove(value2))
+         .then(_ => {
+            expect(coll.length).to.be.equal(2);
+            expect(coll.at(0)).to.be.equal(value1);
+            expect(coll.at(1)).to.be.equal(value3);
+         })
+         .then(_ => done())
+         .catch(err => done(err));
+   })
+
+   it('removeAt()', function(done) {
+      let coll = collection.value(types.string.type());
+      let value1 = string.value('one');
+      let value2 = string.value('two');
+      let value3 = string.value('three');
+
+      coll.add(value1, value2, value3)
+         .then(_ => expect(coll.length).to.be.equal(3))
+         .then(_ => coll.removeAt(1))
+         .then(_ => {
+            expect(coll.length).to.be.equal(2);
+            expect(coll.at(0)).to.be.equal(value1);
+            expect(coll.at(1)).to.be.equal(value3);
+         })
+         .then(_ => done())
+         .catch(err => done(err));
+   })
+
+   it('removeAll()', function(done) {
+      let coll = collection.value(types.string.type());
+      let value1 = string.value('one');
+      let value2 = string.value('two');
+      let value3 = string.value('three');
+
+      coll.add(value1, value2, value3)
+         .then(_ => expect(coll.length).to.be.equal(3))
+         .then(_ => coll.removeAll(v => v.equals(value2)))
+         .then(_ => {
+            expect(coll.length).to.be.equal(2);
+            expect(coll.at(0)).to.be.equal(value1);
+            expect(coll.at(1)).to.be.equal(value3);
+         })
+         .then(_ => done())
+         .catch(err => done(err));
+   })
+
+
+
+
+
+   describe('#events', function() {
+
+   });
+
+   // it(`should raise a ${events.add} event`, function(done) {
+   //    types.collection.value(types.int.type());
+   //    value.on(Events.valueChanged, spy);
+   //    proxy.changeValue(value);
+   //    setTimeout(() => { expect(spy).to.have.been.called(); done(); }, 25);
+   // });
 }
 
 describe('Types:', function () {
@@ -159,5 +347,7 @@ describe('Types:', function () {
 
    describe('# collection', function () {
       sharedBehaviorTest(collProxy);
+      // Next: test the collection API
+      collectionTests(collProxy);
    });
 });
